3ee7176e2ebec9e669301a929cb0948a1f4b297a
Maximiliano Dalla Porta <gigamax13@gmail.com>
2024-11-15 05:20:25 -0300

    chore: move resume-short-version

--- _scripts/buid-pdf.sh
+++ _scripts/buid-pdf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc resume-short-version.md -o ./assets/resume.pdf --css pandoc-style.css --pdf-engine=wkhtmltopdf; open assets/resume.pdf
+pandoc ./_scripts/resume-short-version.md -o ./assets/resume.pdf --css ./_scripts/pandoc-style.css --pdf-engine=wkhtmltopdf; open assets/resume.pdf
